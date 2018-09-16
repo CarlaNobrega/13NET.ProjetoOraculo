637a91f5c946eaf09d8151341a6eb6962ae37a64
CarlaNobrega <carlanobrega22@gmail.com>
2018-09-16 18:06:25 -0300

    Commit Inicial

---
+++ BotResposta.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using StackExchange.Redis;
+using System.Threading.Tasks;
+
+namespace _13NET.ProjetoOraculo
+{
+    public class BotResposta
+    {
+        const string CHANNEL_PERGUNTAS = "perguntas";
+        private readonly IDatabase _db;
+        private readonly ISubscriber _pub;
+
+
+        public BotResposta(string connectionString)
+        {
+            var client = ConnectionMultiplexer.Connect(connectionString);
+            var db = client.GetDatabase();
+            var pub = client.GetSubscriber();
+
+            this._db = db;
+            this._pub = pub;
+        }
+
+        public void Responder(int id, string nomeGrupo, string texto)
+        {
+            string idPergunta = $"P{id}";
+            
+            bool wasSet = _db.HashSet(idPergunta, nomeGrupo, texto, When.NotExists);
+        }
+
+        public async Task<Pergunta> EsperarPerguntaAsync(int id)
+        {            
+            await Task.Delay(1000);
+
+            var pergunta = LerPergunta(id);
+            if (pergunta != null)
+            {
+                return pergunta;
+            }
+            
+            while (pergunta == null)
+            {
+                await Task.Delay(2000);
+                pergunta = LerPergunta(id);
+            }            
+
+            return pergunta;
+        }
+
+        private Pergunta LerPergunta(int id)
+        {
+            string perguntaId = "P" + id.ToString();
+
+            var perguntaCompleta = _db.StringGet(perguntaId).ToString();
+
+            if (string.IsNullOrEmpty(perguntaCompleta))
+            {
+                return null;
+            }
+
+            var perguntaSplit = perguntaCompleta.Split(':');           
+
+            Pergunta pergunta = new Pergunta { ID = perguntaSplit[0], Texto = perguntaSplit[1] };
+
+            return pergunta;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,40 @@ namespace _13NET.ProjetoOraculo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Respostas");
+
+            string connectionString = (args.Length > 0) ? args[0] : "localhost";
+            var botRespostas = new BotResposta(connectionString);
+
+
+            for (int i = 1; ; i++)
+            {
+                var pergunta = botRespostas.EsperarPerguntaAsync(i).Result;
+
+                if (pergunta == null)
+                {
+                    Console.WriteLine("Nenhuma pergunta");
+                }
+                else
+                {
+
+                    Console.WriteLine($"  - [{pergunta.ID}] : {pergunta.Texto}");
+
+                    Console.WriteLine("Dê uma resposta para a pergunta: ");
+
+                    string texto = Console.ReadLine();
+
+                    if (texto == null || texto == "")
+                        break;
+
+                    botRespostas.Responder(i, "GRUPO1", texto);
+
+                }
+
+                Console.WriteLine("----------------------------");
+            }
+
+
         }
     }
 }
